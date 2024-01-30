301822433eddf763fa8282e0ef9b35e98c6d0484
Allan Njoroge <allcreativewrites@gmail.com>
2024-01-30 18:10:34 +0300

    modified

--- client/src/Components/UserProfile/Profile.jsx
+++ client/src/Components/UserProfile/Profile.jsx
@@ -1,33 +1,42 @@
 import React from "react";
 import "./profile.css";
-import Header from "./Header";
+import Header from "../Header/Header";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import PostView from "../Dashboard/PostView";
+import profile from "../../assets/profile.jpg";
 
 function Profile() {
+  const currentUser = JSON.parse(localStorage.getItem("user"));
+
   return (
     <div className="profile-container">
       <Header />
       <div className="cols">
         <div className="left-col">
           <div className="img-container">
-            <img
-              src="https://images.unsplash.com/photo-1706018167918-a1b9ef373eac?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-              alt=""
-            />
+            <img src={profile} alt="" />
             <span></span>
           </div>
-          <h2>Allan Njoroge</h2>
+          <h2>{currentUser.username}</h2>
           <p>Software Engineer</p>
-          <p>allan.njoroge@student.moringaschool.com</p>
+          <p>{currentUser.email}</p>
 
           <ul className="user-about">
             <li>
-              <span>1000</span>Likes
+              <span>
+                {currentUser.votes
+                  ? currentUser.votes.filter((vote) => {
+                      return vote.vote_type === true;
+                    }).length
+                  : 0}
+              </span>
+              Upvotes
             </li>
             <li>
-              <span>25</span>Posts
+              <span>{currentUser.posts ? currentUser.posts.length : 0}</span>
+              Posts
             </li>
           </ul>
           <div className="user-bio">
@@ -61,7 +70,22 @@ function Profile() {
               <li>My posts</li>
             </ul>
           </nav>
-          <div className="posts"></div>
+          <div className="posts">
+            {currentUser.posts
+              ? currentUser.posts.map((post) => {
+                  return (
+                    <div className="post-cards" key={post.id}>
+                      <h6 className="post-title">{post.title}</h6>
+                      <p>{post.content.slice(0, 250) + "..."}</p>
+                      <hr />
+                      <span>
+                        Posted on: <em>{post.created_at}</em>
+                      </span>
+                    </div>
+                  );
+                })
+              : null}
+          </div>
         </div>
       </div>
     </div>
