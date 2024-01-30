e277c8ce5be2a19d5572be9814e09a5b122e6f42
Allan Njoroge <allcreativewrites@gmail.com>
2024-01-30 18:11:27 +0300

    Client in progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Signin from "./Components/SignIn/SignIn";
@@ -7,15 +7,35 @@ import Dashboard from "./Components/Dashboard/Dashboard";
 import Create from "./Components/Create/Create";
 import { useGlobalUserContext } from "./context/authContext";
 import Profile from "./Components/UserProfile/Profile";
+import PostPage from "./Components/Post/PostPage";
+import UserAccountPage from "./Components/UserAccount/AccountPage";
 
 function App() {
-  const { setToken } = useGlobalUserContext();
+  // const [userToken, setUserToken] = useState("");
+  // const [user, setUser] = useState(null);
+  // function fetchUser(id) {
+  //   if (id) {
+  //     fetch(`/users/${id}`)
+  //       .then((r) => r.json())
+  //       .then((data) => {
+  //         // save user data in state and local storage
+  //         setUser(data);
+  //         localStorage.setItem("user", JSON.stringify(data));
+  //         //save user token on local storage
+  //         localStorage.setItem("token", userToken);
+  //       });
+  //   }
+  // }
 
-  useEffect(() => {
-    fetch("/check_session")
-      .then((resp) => resp.json())
-      .then((data) => setToken(data.token));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/check_session")
+  //     .then((resp) => resp.json())
+  //     .then((data) => {
+  //       //setuserToken(data.token);
+  //       setUserToken(data.token);
+  //       fetchUser(data.user_id);
+  //     });
+  // }, []);
 
   return (
     <div>
@@ -26,6 +46,8 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} exact />
         <Route path="/create" element={<Create />} exact />
         <Route path="/profile" element={<Profile />} exact />
+        <Route path="/posts/:id" element={<PostPage />} />
+        <Route path="/account" element={<UserAccountPage />} />
       </Routes>
     </div>
   );
--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -1,28 +1,27 @@
 import React, { useContext, useState } from "react";
 
-const userContext = React.createContext()
+const userContext = React.createContext();
 
-const UserProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState({})
-    const [token, setToken] = useState('')
-    return (
-        <userContext.Provider
-            value={
-                {
-                    currentUser,
-                    setCurrentUser,
-                    token,
-                    setToken,
-                }            
-            }
-        >
-            {children}
-        </userContext.Provider>
-    )
-}
+const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState({});
+  const [token, setToken] = useState("");
+
+  return (
+    <userContext.Provider
+      value={{
+        currentUser,
+        setCurrentUser,
+        token,
+        setToken,
+      }}
+    >
+      {children}
+    </userContext.Provider>
+  );
+};
 
 const useGlobalUserContext = () => {
-    return useContext(userContext)
-}
+  return useContext(userContext);
+};
 
-export {UserProvider, useGlobalUserContext}
+export { UserProvider, useGlobalUserContext };
